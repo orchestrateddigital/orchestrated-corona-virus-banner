b31ef475678e3be988931da9a1626ffe734767df
Nick Tomkin <nick@orchestrated.ca>
2020-03-17 11:39:00 -0400

    Bug fix for vendor library inclusion

--- create_distribution.sh
+++ create_distribution.sh
@@ -3,17 +3,18 @@
 BASE=.
 
 mkdir $BASE/dist
+rm $BASE/latest.zip
 cp -Rf $BASE/src/* $BASE/dist/
 rm $BASE/dist/assets/css/*.scss
 rm $BASE/dist/assets/js/admin.js
 rm $BASE/dist/assets/js/frontend.js
 rm $BASE/dist/assets/js/settings.js
-rm -Rf $BASE/dist/vendor
 rm -Rf $BASE/dist/node_modules
 rm $BASE/dist/package*.json
 rm $BASE/dist/Gruntfile.js
 rm $BASE/dist/readme.md
 rm $BASE/dist/composer.json
 
-zip -r -X $BASE/latest.zip $BASE/dist/
-rm -Rf $BASE/dist
+mv $BASE/dist $BASE/orchestrated-corona-virus-banner
+zip -r -X $BASE/latest.zip $BASE/orchestrated-corona-virus-banner/
+rm -Rf $BASE/orchestrated-corona-virus-banner
