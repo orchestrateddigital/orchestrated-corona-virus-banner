555704c382d639ca28d52709f782cc005dc674b2
Nick Tomkin <nick@orchestrated.ca>
2020-03-17 11:23:30 -0400

    Updated distribution script, removed unnecessary libraries

--- create_distribution.sh
+++ create_distribution.sh
@@ -9,10 +9,11 @@ rm $BASE/dist/assets/js/admin.js
 rm $BASE/dist/assets/js/frontend.js
 rm $BASE/dist/assets/js/settings.js
 rm -Rf $BASE/dist/vendor
+rm -Rf $BASE/dist/node_modules
 rm $BASE/dist/package*.json
 rm $BASE/dist/Gruntfile.js
 rm $BASE/dist/readme.md
 rm $BASE/dist/composer.json
 
-zip -r -X $BASE/plugin.zip $BASE/src/
+zip -r -X $BASE/latest.zip $BASE/dist/
 rm -Rf $BASE/dist
